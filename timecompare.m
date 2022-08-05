e8f6fb9a248bef960031f246dbc27c00fcbd5496
JeonghoonJu <68767959+JeonghoonJu@users.noreply.github.com>
2022-08-05 17:36:49 +0900

    Update timecompare.m

--- timecompare.m
+++ timecompare.m
@@ -1,8 +1,8 @@
 %Code for Comparing Times in Example1,2
 clear
 clc
-n=10; 
-m=100; %Change the size of matrices m
+n=50; 
+m=500; %Change the size of matrices m
 
 [A, B, C]=makeABCdampKim(m);
 B=0.95*B;
@@ -25,7 +25,7 @@ for i=1:n
     c(i,1)=t;
     clear t
     
-    %Cyclic Reduction2
+    %Cyclic Reduction
     [X, iter, res, t] = cr_uqme(A,B,C);
     d(i,1)=t;
     clear t
@@ -44,6 +44,6 @@ end
 disp('time BI1='); sum(a)/n
 disp('time BI2'); sum(b)/n
 disp('time Newton'); sum(c)/n
-disp('time cyclic2'); sum(d)/n
+disp('time cyclic'); sum(d)/n
 disp('time BI1-OC'); sum(e)/n
 disp('time BI2-OC'); sum(f)/n
