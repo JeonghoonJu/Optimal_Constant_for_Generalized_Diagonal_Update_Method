0f81a153da11eb6de6c4bf3c4312f134e590df78
JeonghoonJu <68767959+JeonghoonJu@users.noreply.github.com>
2022-08-05 17:37:52 +0900

    Update cr_uqme.m

--- cr_uqme.m
+++ cr_uqme.m
@@ -2,35 +2,38 @@ function [X,iter,res,t]= cr_uqme(A,B,C)
 %Code for Cyclic Reduction Method
 %X : primary solvent of AX^2+BX+C=0 (when it is overdamped)
 
-tol = 1e-13;
+tol = 5e-15;
 kmax = 30;
 err = 1;
+m = size(C,1);
 
 X0=B;
 A0=A;
 B0=B;
 C0=C;
 
-
 iter = 0;
 tic
-while (err > tol) && (iter < kmax)
-    X1=X0-A0*inv(B0)*C0;
-    X=-inv(X1)*C;
-    err = norm( X^2 + B*X + C,'fro');    
+while (err > m*tol) && (iter < kmax)
+    W0=B0\[A0 C0];
+    W1=W0(:,1:m); %W1=inv(B0)*A0
+    W2=W0(:,m+1:2*m); %W2=inv(B0)*C0
+
+    W=C0*W1; %W=C0*inv(B0)*A0
+    C0=C0*W2; %C0=C0*inv(B0)*C0
+    B0=B0-W; %B0=B0-C0*inv(B0)*A0
     
-    A1=A0*inv(B0)*A0;
-    B1=B0-A0*inv(B0)*C0-C0*inv(B0)*A0;
-    C1=C0*inv(B0)*C0;
+    W=A0*W2; %W=A0*inv(B0)*C0
+    A0=A0*W1; %A0=A0*inv(B0)*A0
+    X0=X0-W; %X0=X0-A0*inv(B0)*C0
+    B0=B0-W; %B0=B0-C0*inv(B0)*A0-A0*inv(B0)*C0
     
+    err = norm(A0,1);
     iter = iter + 1;
-    A0=A1;
-    B0=B1;
-    C0=C1;
-    X0=X1;
 end
 if iter == kmax
     disp('warning cr')
 end
+X=-X0\C;
 t=toc;
-res=err;
+res=norm( X^2 + B*X + C,'fro');
