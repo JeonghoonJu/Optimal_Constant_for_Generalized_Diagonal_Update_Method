b51428450e247e587d06c3d91f12dadbb94e1055
JeonghoonJu <68767959+JeonghoonJu@users.noreply.github.com>
2023-12-22 06:28:33 +0900

    Update nm_qme_Bartels.m

--- nm_qme_Bartels.m
+++ nm_qme_Bartels.m
@@ -1,36 +1,33 @@
 function [X,iter,res,t]=nm_qme_Bartels(A,B,C)
 % A*X^2 + B*X + C = 0
 
-tol = 1e-13;
+tol = 5e-15;
 kmax = 30;
+err = 1;
+m=size(C,1);
 
-m=size(A);
 X0 = zeros(m);
 B=A\B;
 C=A\C;
-A=eye(m);
-iter = 0;                                                                  
+A=eye(m);                                                                
 I = eye(m);
 H=zeros(m);
-err = 1;
-normA = norm(A,'fro');
-normB = norm(B,'fro');
-normC = norm(C,'fro');
+
+iter = 0;  
 
 tic;
 
-while (err > tol) && (iter < kmax)
+while (err > m*tol) && (iter < kmax)
     H =bartelsStewart(A,X0.', A*X0 + B,I,-X0^2 - B*X0 - C);
-    X1 = X0 + H ;
-    X = X1;
-    err = norm( X1^2 + B*X1 + C,'fro');
+    X0 = X0 + H;
+    err = norm(H,'inf')/norm(X0,'inf');
     
     iter = iter + 1 ;                                                    
-    X0 = X1;
 end
 if iter >= kmax
     disp('warning nm')
 end
 
 t = toc;
-res = err;
+X=X0;
+res = norm(X^2+B*X+C,'inf')/(norm(X^2,'inf')+norm(B*X,'inf')+norm(C,'inf'));
