168888f44191a68e44f93b74a4b582211ad67834
JeonghoonJu <68767959+JeonghoonJu@users.noreply.github.com>
2023-12-22 06:32:42 +0900

    Update bi2_oc.m

--- bi2_oc.m
+++ bi2_oc.m
@@ -1,12 +1,13 @@
 function [X, iter, res, t] = bi2_oc(B,C)
 %Code for Generalized Diagonal Update Method with H_gamma
 
-tol = 5e-15;
+tol =5e-15;
 m=size(B);
 X0 = zeros(m);
 X = X0;
 I = eye(m);
 iter = 0;
+
 tic;
 a=min(real(eig(B-C)))-0.0001;
 % we regard A is idenity 
@@ -14,11 +15,11 @@ a=min(real(eig(B-C)))-0.0001;
 while(1)
     d = min(1,min(abs(diag(X))));
     X = (-B+a*d*I)\(X*X+a*d*X+C);
-    if norm(X-X0,'inf') < m*tol
+    if norm(X-X0,'inf')/norm(X,'inf') < m*tol
         break;
     end
     X0 = X;
     iter = iter + 1;
 end
 t = toc;
-res = norm(X^2+B*X+C,'fro');
+res = norm(X^2+B*X+C,'inf')/(norm(X^2,'inf')+norm(B*X,'inf')+norm(C,'inf'));
