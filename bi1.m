0e3352fc14ba1d66c7cc4088c9e6bbec4f6f4a20
JeonghoonJu <68767959+JeonghoonJu@users.noreply.github.com>
2023-12-22 06:32:58 +0900

    Update bi1.m

--- bi1.m
+++ bi1.m
@@ -7,14 +7,15 @@ X0 = zeros(m);
 X = X0;
 iter = 0;
 % we regard A is idenity 
+
 tic;
 while(1)
     X = -(B+X)\(C);
-    if norm(X-X0,'inf') < m*tol
+    if norm(X-X0,'inf')/norm(X,'inf') < m*tol
         break;
     end
     X0 = X;
     iter = iter + 1;
 end
 t = toc;
-res = norm(X^2+B*X+C,'fro');
+res = norm(X^2+B*X+C,'inf')/(norm(X^2,'inf')+norm(B*X,'inf')+norm(C,'inf'));
