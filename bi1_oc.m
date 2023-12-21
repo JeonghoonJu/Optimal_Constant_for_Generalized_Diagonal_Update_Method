d090736b92f6d3b2d5595158ad0ba96d964d529a
JeonghoonJu <68767959+JeonghoonJu@users.noreply.github.com>
2023-12-22 06:33:17 +0900

    Update bi1_oc.m

--- bi1_oc.m
+++ bi1_oc.m
@@ -1,11 +1,13 @@
 function [X, iter, res, t] = bi1_oc(B,C)
 %Code for Generalized Diagonal Update Method with G_gamma
+
 tol = 5e-15;
 m=size(B);
 X0 = zeros(m);
 X = X0;
 I = eye(m);
 iter = 0;
+
 tic;
 a=min(real(eig(B-C)))-1.0001;
 % we regard A is idenity 
@@ -13,11 +15,11 @@ a=min(real(eig(B-C)))-1.0001;
 while(1)
     d = min(1,min(abs(diag(X))));
     X = -(B+X-a*d*I)\(C+a*d*X);
-    if norm(X-X0,'inf') < m*tol
+    if norm(X-X0,'inf')/norm(X,'inf') < m*tol
         break;
     end
     X0 = X;
     iter = iter + 1;
 end
 t = toc;
-res = norm(X^2+B*X+C,'fro');
+res = norm(X^2+B*X+C,'inf')/(norm(X^2,'inf')+norm(B*X,'inf')+norm(C,'inf'));
